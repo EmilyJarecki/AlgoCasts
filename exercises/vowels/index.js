23202a1e47e98e1713222c6156057c5d9b71b9a3
EmilyJarecki <jareckiemily@yahoo.com>
2023-02-14 20:47:27 -0600

    completed successfully

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// COMPLETED SUCCESSFULLY ON MY OWN
+
+function vowels(str) {
+    let vowels = 0
+    const str1 = str.toLowerCase().split("")
+    for (let i=0; i<str1.length; i++){
+        if (
+            str1[i] === "a" ||
+            str1[i] === "e" ||
+            str1[i] === "i" ||
+            str1[i] === "o" ||
+            str1[i] === "u"
+        ){
+           vowels++
+        }
+    }
+    console.log(vowels)
+}
+
+vowels("Why hello there")
+
 
 module.exports = vowels;
